f831f4b58f7c9bffa6d5251bc8a9a173f96d12e4
Jon Haase <haase1020@gmail.com>
2020-06-17 18:12:56 -0400

    complete product of all other numbers and passing tests

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,28 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
+from functools import reduce
 
-    pass
+
+def product_of_all_other_numbers(arr):
+    # for i in range(len(arr)):
+    #     print(i)
+    productarray = []
+    for i in range(0, len(arr)):
+        # exclude i
+        product = [x for index, x in enumerate(arr) if index != i]
+        # add product of all items but i to new list
+        # reduce(function, iterable, initializer)
+        productarray.append(reduce(lambda x, y: x * y, product, 1))
+
+    return productarray
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+           9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
