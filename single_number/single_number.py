ac2b5bf2c085dffacffd9a10504dacea58cbc919
Jon Haase <haase1020@gmail.com>
2020-06-18 14:07:43 -0400

    start on optimized solutions based on guided lecture

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -15,3 +15,23 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
+
+# list not best choice for efficiency. Following from guided lecture
+def single_number_optimized(nums):
+    # keep track of number of times an items occurs in input
+    counts = {}
+
+    # loop through inpu list O(n)
+    for num in nums:
+        # if item in counts
+        if num in counts:
+            del counts[num]
+
+        else:
+            # set item count to 1
+            counts[num] = 1
+    return counts[counts.keys()[0]]
+    for k, v in counts.items():
+        if v == 1:
+            return k
